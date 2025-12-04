a9d041c73a720c0b09494310f982d6d2ae65f448
jaikersiddharth <jaikersiddharth@email.com>
2025-12-04 19:29:38 +0530

    fixed workflow

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import {resolve,dirname} from "path";
 import { fileURLToPath } from 'url';
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '#components':resolve(dirname(fileURLToPath(import.meta.url)),'src/components'),
